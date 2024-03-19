b6536c73fdf8ba347849d6f9f5744bbae41d23ba
Guanwei Liu <toyjack@gmail.com>
2024-03-19 15:17:44 +0900

    change default theme to light mode

--- src/lib/atoms.ts
+++ src/lib/atoms.ts
@@ -3,4 +3,4 @@ import {atom, createStore} from "jotai";
 export const store = createStore();
 
 export const perPageAtom = atom(100);
-export const themeAtom = atom("dark");
+export const themeAtom = atom("light");
