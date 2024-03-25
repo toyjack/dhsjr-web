ebaa508d047adbcaf36e4df908ea79ba314c0b53
Guanwei Liu <toyjack@gmail.com>
2024-03-25 17:09:27 +0900

    added link for characters, books, words

--- src/components/results-table.tsx
+++ src/components/results-table.tsx
@@ -2,6 +2,7 @@
 
 import { Dhsjr } from "@prisma/client";
 import RubyCell from "./ruby-cell";
+import Link from "next/link";
 
 export default function ResultsTable({ data }: { data: Dhsjr[] }) {
   return (
@@ -12,23 +13,63 @@ export default function ResultsTable({ data }: { data: Dhsjr[] }) {
             <th>character_id</th>
             <th>book_name</th>
             <th>
-              <RubyCell baseText="character(fanqie, ruion)" rubyTop="shoten" rubyBottom="kana" />
+              <RubyCell
+                baseText="character(fanqie, ruion)"
+                rubyTop="shoten"
+                rubyBottom="kana"
+              />
             </th>
             <th>
-              <RubyCell baseText="word" rubyTop="shoten_word" rubyBottom="word_kana" />
+              <RubyCell
+                baseText="word"
+                rubyTop="shoten_word"
+                rubyBottom="word_kana"
+              />
             </th>
           </tr>
         </thead>
         <tbody>
           {data.map((row) => (
             <tr key={row.character_id}>
-              <th>{row.character_id}</th>
-              <td>{row.book_name}</td>
+              <th>
+                <Link
+                  href={`/character/${row.character_id}`}
+                  className="link link-hover"
+                >
+                  {row.character_id}
+                </Link>
+              </th>
+              <td>
+                <div>
+                  <div>
+                    <Link
+                      className="link link-hover"
+                      href={`/book/${row.book_id}`}
+                    >
+                      {row.book_name}
+                    </Link>
+                  </div>
+                  <div>{row.position_in_book}</div>
+                </div>
+              </td>
               <td className="text-base-content text-2xl font-semibold">
-                <RubyCell baseText={row.character} rubyTop={row.shoten} rubyBottom={row.kana} fanqie={row.fanqie} ruion={row.ruion} />
+                <RubyCell
+                  baseText={row.character}
+                  rubyTop={row.shoten}
+                  rubyBottom={row.kana}
+                  fanqie={row.fanqie}
+                  ruion={row.ruion}
+                />
               </td>
               <td className="text-2xl">
-                <RubyCell baseText={row.word} rubyTop={row.shoten_word} rubyBottom={row.word_kana}  />
+                {/* TODO word's id  */}
+                <Link href={`/word/${row.book_id}-${row.word_index_in_book}`}>
+                  <RubyCell
+                    baseText={row.word}
+                    rubyTop={row.shoten_word}
+                    rubyBottom={row.word_kana}
+                  />
+                </Link>
               </td>
             </tr>
           ))}
