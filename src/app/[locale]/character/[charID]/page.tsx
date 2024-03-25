a4e6b7b51d9d6aad6ae4a42f0de8b8ee500ebb93
Guanwei Liu <toyjack@gmail.com>
2024-03-25 17:16:26 +0900

    workingon

--- src/app/[locale]/character/[charID]/page.tsx
+++ src/app/[locale]/character/[charID]/page.tsx
@@ -1,9 +1,21 @@
+import { prisma } from "@/lib/prisma";
 import React from "react";
 
-export default function CharacterPage({
+export default async function CharacterPage({
   params,
 }: {
   params: { charID: string };
 }) {
-  return <div>CharacterPage: {params.charID}</div>;
+  const character = await prisma.dhsjr.findUnique({
+    where:{
+      character_id: params.charID
+    }
+  })
+  
+  return <div>
+    <div>CharacterPage: {params.charID}</div>
+    <pre>
+      {JSON.stringify(character, null, 2)}
+    </pre>
+  </div>;
 }
