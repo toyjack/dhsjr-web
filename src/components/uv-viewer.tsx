67b98c0b9dd8e7cd68f3d9d888c014a76446ef58
toyjack <toyjack@gmail.com>
2024-10-10 16:11:52 +0900

    update

--- src/components/uv-viewer.tsx
+++ src/components/uv-viewer.tsx
@@ -5,17 +5,16 @@ import Script from 'next/script'
 export default function UvViewer({manifestUrl}: {manifestUrl: string}) {
   return (
     <div>
-      <div id="uv" style={{minHeight:"90vh"}}></div>
+      <div id="uv" style={{minHeight:"90vh"}} />
       <Script
         id="uv"
         src="https://cdn.jsdelivr.net/npm/universalviewer@4.0.0/dist/umd/UV.js"
-        onReady={() => {
+        onLoad={() => {
           const data = {
             manifest: manifestUrl,
-            embedded: true // needed for codesandbox frame
           };
           // @ts-ignore
-          uv = UV.init("uv", data);
+          const uv = UV.init("uv", data);
         }}
       />
     </div>
